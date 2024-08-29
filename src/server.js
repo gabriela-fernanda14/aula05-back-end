d6bbbd7c1e88f425a99f620bb251ec0159afba89
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-08-29 11:25:25 -0300

    feat: add create emotion request

--- src/server.js
+++ src/server.js
@@ -66,6 +66,17 @@ app.get("/personagens", (req, res) => {
     return res.status(200).send(personagens)
 })
 
+app.post("/emocoes", (req, res) => {
+    const { nome, cor} = req.body
+    const novaEmocao ={
+        id: emocoes.length + 1,
+        nome: nome,
+        cor: cor
+    }
+    emocoes.push(novaEmocao)
+    return res.status(200).send(emocoes)
+})
+
 app.listen(serverPort, () => {
     console.log(`💜 Server started on http://localhost:${serverPort}`)
 })
