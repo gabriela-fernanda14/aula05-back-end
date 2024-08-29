15fad7b9f973f3d191ebc31332559a921b72f91a
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-08-29 10:53:34 -0300

    feat: add some request

--- src/server.js
+++ src/server.js
@@ -3,10 +3,52 @@ import {config} from 'dotenv'
 
 config()
 
-const serverPort = process.env.PORT || 3000
+const serverPort = process.env.PORT || 4000
 
 const app = express()
 app.use (express.json())
+
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+    {
+        id: 2,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+    {
+        id: 3,
+        nome: "Vergonha",
+        cor: "Rosa"
+    }
+]
+
+const personagens = [
+    {
+        id:100,
+        nome: "mickey",
+        vivo: true,
+        studio: "Disney"
+    },
+
+    {
+        id:101,
+        nome: "minnie",
+        vivo: true,
+        studio: "Disney"
+    },
+
+    {
+        id:102,
+        nome: "Pluto",
+        vivo: true,
+        studio: "Disney"
+    }
+]
+
 
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
@@ -14,6 +56,14 @@ app.get("/", (req, res) => {
 
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
+})
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send(emocoes)
+})
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send(personagens)
 })
 
 app.listen(serverPort, () => {
