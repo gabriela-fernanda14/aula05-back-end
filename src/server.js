05ad745bbb75ec235a09bbfd981a5babd6569129
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-09-05 15:41:39 -0300

    feat: create routes configs

--- src/server.js
+++ src/server.js
@@ -1,25 +1,16 @@
-import express from 'express'
-import {config} from 'dotenv'
+import express from 'express';
+import {config} from 'dotenv';
+
+import routesfro from './routes/index.routes.js';
+import routes from './routes/index.routes.js';
 
 config()
 
 const serverPort = process.env.PORT || 4000
 
-const app = express()
-app.use (express.json())
-
-app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!"})
-})
-
-app.get("/2tds2", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!"})
-})
-
-
-app.get("/personagens", (req, res) => {
-    return res.status(200).send(personagens)
-})
+const app = express();
+app.use (express.json());
+app.use (routes);
 
 app.listen(serverPort, () => {
     console.log(`💜 Server started on http://localhost:${serverPort}`)
