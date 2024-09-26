42ec79e65119355aadd3df58fe090d4f806e78ba
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-09-26 14:50:50 -0300

    Create update emotion

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -18,12 +18,12 @@ let emocoes = [
         nome: "Vergonha",
         cor: "Rosa"
     }
-]
-
+];
+//rota para buscar todas as emoções
 emocoesRoutes.get("/", (req, res) => {
     return res.status(200).send(emocoes)
 })
-
+//rota para criar uma nova emoção
 emocoesRoutes.post("/", (req, res) => {
     const { nome, cor} = req.body
     const novaEmocao ={
@@ -35,5 +35,52 @@ emocoesRoutes.post("/", (req, res) => {
     emocoes.push(novaEmocao)
     return res.status(201).send(emocoes)
 });
+
+emocoesRoutes.get("/:id", (req, res) => {
+    const {id} = req.params;
+
+    //console.log(id);
+    
+    const emocao = emocoes.find((emotion) => emotion.id == id)
+
+    if (!emocao) {
+        return res.status (404) .send ({
+            message: "Emoção não encontrada",
+        });
+    }
+
+    return res.status (200).send({
+        message: "Emoção encontrada!",
+        emocao,
+    });
+});
+
+emocoesRoutes.put("/:id", (req, res) => {
+    const {id} = req.params;
+
+    const emocao = emocoes.find((emotion) => emotion.id == id)
+
+    if (!emocao) {
+        return res.status (404) .send ({
+            message: "Emoção não encontrada",
+        });
+    }
+
+    const { nome, cor} = req.body
+    emocao.nome = nome;
+    emocao.cor = cor;
+
+    return res.status (200).send({
+        message: " Emoção Atualizada!",
+        emocao,
+    });
+
+
+
+
+
+
+})
+
 
 export default emocoesRoutes
